eddea6b7b400a946193c2f0169814e7261497556
gau94rav <gau94rav@gmail.com>
2021-12-17 18:13:22 +0530

    refactor(TextInput): refactor styles data variables & tailwind class order

--- src/components/TTextInput.vue
+++ src/components/TTextInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-3 overflow-hidden h-max w-full truncate whitespace-nowrap">
+  <div class="w-full py-3 overflow-hidden truncate h-max whitespace-nowrap">
     <div
       class="relative flex items-center pb-0.5 w-full"
       :class="[
@@ -8,22 +8,22 @@
           'border-error-400 placeholder-error-400': isTrue(isError),
           'border-primary-400': !isTrue(isError) && isFocused,
           'border-neutral-400': !isTrue(isError) && !isFocused,
-          [borderWidthSizes[borderWidth]]: borderWidth && isTrue(isOutlined),
-          [colors[innerBgColor]]: isTrue(isOutlined),
-          [borderRadius[rounded]]: isTrue(isOutlined),
+          [styles.borderWidthSizes[borderWidth]]: borderWidth && isTrue(isOutlined),
+          [styles.colors[innerBgColor]]: isTrue(isOutlined),
+          [styles.borderRadius[rounded]]: isTrue(isOutlined),
         },
         isTrue(isOutlined) ? 'border' : 'border-b',
-        borders[borderStyle],
+        styles.borders[borderStyle],
       ]"
     >
       <!-- Label -->
       <div class="flex flex-col justify-center w-full">
         <div
-          class="text-left w-max transition-colors"
+          class="text-left transition-colors w-max"
           :class="{ 'text-error-400': isTrue(isError), 'pt-3': insideLabel }"
         >
           <!-- Inside Label -->
-          <label :for="inputId" class="pb-1 px-1 ml-4.5 text-sm top-0" v-if="insideLabel">
+          <label :for="inputId" class="pb-1 px-1 ml-4.5 text-sm top-0 opacity-80" v-if="insideLabel">
             {{ label }}
           </label>
 
@@ -34,8 +34,8 @@
             :for="inputId"
             :class="[
               // Translate label according to border-width.
-              topLabel ? borderLabelPosition[borderWidth] : 'translate-y-4 h-max',
-              { [colors[innerBgColor]]: isTrue(isOutlined) },
+              topLabel ? styles.borderLabelPosition[borderWidth] : 'translate-y-4 h-max',
+              { [styles.colors[innerBgColor]]: isTrue(isOutlined) },
             ]"
           >
             <div :class="{ 'relative bottom-1.5': topLabel }" class="relative">
@@ -51,14 +51,14 @@
           :id="inputId"
           :disabled="isTrue(isDisabled)"
           :placeholder="placeholder"
-          :class="[borderRadius[rounded], insideLabel ? 'py-1 pb-3' : 'py-4']"
+          :class="[styles.borderRadius[rounded], insideLabel ? 'py-1 pb-3' : 'py-4']"
           @focus="isFocused = true"
           @blur="isFocused = false"
         />
       </div>
 
       <!-- Icon -->
-      <div class="pr-3 pt-2 absolute right-0 top-0 bottom-0 my-auto h-max">
+      <div class="absolute top-0 bottom-0 right-0 pt-2 pr-3 my-auto h-max">
         <i
           v-if="(value && isTrue(isClearable)) || isTrue(isLoading)"
           class="w-max"
@@ -157,39 +157,41 @@ export default {
     return {
       isFocused: false, // true if input has value or typing cursor
       value: '',
-      borders: {
-        solid: 'border-solid',
-        dashed: 'border-dashed',
-        dotted: 'border-dotted',
-        double: 'border-double',
-      },
-      borderRadius: {
-        none: 'rouned-none',
-        true: 'rounded',
-        sm: 'rounded-sm',
-        md: 'rounded-md',
-        lg: 'rounded-lg',
-        xl: 'rounded-xl',
-        '2xl': 'rounded-2xl',
-        '3xl': 'rounded-3xl',
-      },
-      borderWidthSizes: {
-        1: 'border',
-        2: 'border-2',
-        4: 'border-4',
-        8: 'border-8',
-      },
-      borderLabelPosition: {
-        1: '-translate-y-0.5 text-xs',
-        2: '-translate-y-0.5 text-xs',
-        4: '-translate-y-1 text-xs',
-        8: '-translate-y-2 text-sm',
-      },
-      colors: {
-        primary: 'bg-primary-50 text-neutral-900',
-        secondary: 'bg-secondary-50 text-neutral-900',
-        light: 'bg-neutral-50 text-neutral-900',
-        dark: 'bg-neutral-900 text-neutral-50',
+      styles: {
+        borders: {
+          solid: 'border-solid',
+          dashed: 'border-dashed',
+          dotted: 'border-dotted',
+          double: 'border-double',
+        },
+        borderRadius: {
+          none: 'rouned-none',
+          true: 'rounded',
+          sm: 'rounded-sm',
+          md: 'rounded-md',
+          lg: 'rounded-lg',
+          xl: 'rounded-xl',
+          '2xl': 'rounded-2xl',
+          '3xl': 'rounded-3xl',
+        },
+        borderWidthSizes: {
+          1: 'border',
+          2: 'border-2',
+          4: 'border-4',
+          8: 'border-8',
+        },
+        borderLabelPosition: {
+          1: '-translate-y-0.5 text-xs',
+          2: '-translate-y-0.5 text-xs',
+          4: '-translate-y-1 text-xs',
+          8: '-translate-y-2 text-sm',
+        },
+        colors: {
+          primary: 'bg-primary-50 text-neutral-900',
+          secondary: 'bg-secondary-50 text-neutral-900',
+          light: 'bg-neutral-50 text-neutral-900',
+          dark: 'bg-neutral-900 text-neutral-50',
+        },
       },
     }
   },
